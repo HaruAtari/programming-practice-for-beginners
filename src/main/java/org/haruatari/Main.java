652f3ed241032e08925401f5b886c8238995c8da
Viktor Pikaev <vikp@boozt.com>
2023-02-25 13:42:56 +0100

    Init

---
+++ src/main/java/org/haruatari/Main.java
@@ -0,0 +1,7 @@
+package org.haruatari;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("See a README.md file in the root of the repository for a list of tasks.");
+    }
+}
