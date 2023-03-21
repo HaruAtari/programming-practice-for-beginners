9377e28c5180ee28f3b5a5bee759ef9bcc782485
Viktor Pikaev <vikp@boozt.com>
2023-03-21 20:48:36 +0100

    Fix

--- src/com/haruatari/task28/tests/Test.java
+++ src/com/haruatari/task28/tests/Test.java
@@ -7,7 +7,7 @@ import com.haruatari._src.tests.MethodTest;
 import java.util.ArrayList;
 import java.util.List;
 
-@TaskTest(278)
+@TaskTest(28)
 public final class Test extends ClassTest {
     public static void main(String[] args) {
         new Test().run();
